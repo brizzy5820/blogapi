025a69e01264fe243fd075de28eddea2b18ce724
brizzy5820 <egbeyemiboluwatifedavid@gmail.com>
2026-06-01 15:03:58 +0100

    update

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.urlencoded({extended:true}));
-const port = 5500;
+const port = process.env.PORT || 5500;
 const host = '127.0.0.1';
 //  ROutes
 app.use("/api/products",productRoutes);
